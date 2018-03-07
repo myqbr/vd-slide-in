df4c3ede487ac4e12e1b63b9b83af6c6f0aeb22c
myqbr <mpang_89@outlook.com>
2018-03-07 16:23:50 -0300

    Estilos no modal

--- demo/js/vd-slide-in.js
+++ demo/js/vd-slide-in.js
@@ -61,7 +61,7 @@ angular.module('vivadecora.module.vd-slide-in').run(['$templateCache', function(
   'use strict';
 
   $templateCache.put('src/html/vd-slide-in.template.html',
-    "<div class=\"vd-slide-in\"><button class=\"vd-slide-in__dismiss\">&times;</button><div ng-if=\"true\" class=\"vd-slide-in__content\"><h3 class=\"vd-slide-in__content__title\">RECEBA DICAS DE DECORAÇÃO</h3><p class=\"vd-slide-in__content__description\">Ideias, dicas e tudo que precisa saber para decorar sua casa</p><input type=\"text\" class=\"vd-slide-in__content__input\" placeholder=\"Digite seu e-mail\"> <input type=\"submit\" class=\"vd-slide-in__content__button\" value=\"ME ENVIE IDEIAS >\"></div><div ng-if=\"false\" class=\"vd-slide-in__content\"><p class=\"vd-slide-in__content__description\">Obrigada!</p></div></div>"
+    "<div class=\"vd-slide-in\"><div class=\"vd-slide-in__container\"><button class=\"vd-slide-in__dismiss\">&times;</button><div ng-if=\"true\" class=\"vd-slide-in__content\"><h3 class=\"vd-slide-in__content__title\">RECEBA DICAS DE DECORAÇÃO</h3><p class=\"vd-slide-in__content__description\">Ideias, dicas e tudo que precisa saber para decorar sua casa</p><input type=\"text\" class=\"vd-slide-in__content__input\" placeholder=\"Digite seu e-mail\"> <input type=\"submit\" class=\"vd-slide-in__content__button\" value=\"ME ENVIE IDEIAS >\"></div><div ng-if=\"false\" class=\"vd-slide-in__content\"><p class=\"vd-slide-in__content__title\">Obrigada!</p></div></div></div>"
   );
 
 }]);
